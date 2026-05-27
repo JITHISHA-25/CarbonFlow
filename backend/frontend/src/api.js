5d0330b3043be52135e9b146815f75c6abde5ff1
JITHISHA K V <jithishakv47@gmail.com>
2026-05-27 19:56:46 +0530

    Connected frontend to live backend

--- backend/frontend/src/api.js
+++ backend/frontend/src/api.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL:
-    "https://carbonflow-i8lv.onrender.com.onrender.com/api/",
+    "https://carbonflow-i8lv.onrender.com/api/",
 });
